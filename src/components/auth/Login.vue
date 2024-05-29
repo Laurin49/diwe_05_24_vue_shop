79426c2642140486d454c02fabf5d43f14a47bd5
Laurin49 <welter@diwe.info>
2024-05-29 15:57:12 +0200

    dev02 firebase registrieren und login

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -12,6 +12,9 @@
         >
       </p>
     </div>
+    <div class="alert alert-danger col-md-8 offset-2" v-if="error">
+      {{ errorDisplayText }}
+    </div>
     <Form @submit="submitData" :validation-schema="schema" v-slot="{ errors }">
       <div class="form-row">
         <div class="form-group col-md-8 offset-2">
@@ -46,7 +49,10 @@
       <div class="form-row mt-3">
         <div class="form-group col-md-8 offset-2">
           <div class="d-grid">
-            <button class="btn bg-vue">Einloggen</button>
+            <button class="btn bg-vue">
+              <span v-if="!isLoading">Einloggen</span>
+              <span v-else class="spinner-border spinner-border-sm"></span>
+            </button>
           </div>
         </div>
       </div>
@@ -56,6 +62,8 @@
 <script>
 import { Form, Field } from "vee-validate";
 import * as yup from "yup";
+import axios from "axios";
+import { FIREBASE_API_KEY } from "../../config/firebase";
 
 export default {
   name: "Login",
@@ -85,11 +93,46 @@ export default {
     });
     return {
       schema,
+      error: "",
+      isLoading: false,
     };
+  },
+  computed: {
+    errorDisplayText() {
+      if (this.error) {
+        if (this.error.includes("EMAIL_NOT_FOUND")) {
+          return "Die EMail Adresse wurde nicht gefunden";
+        } else if (this.error.includes("INVALID_LOGIN_CREDENTIALS")) {
+          return "Ungültige Angabe der Login - Daten";
+        } 
+        return "Unbekannte Fehler. Bitte versuchen Sie es noch einmal";
+      }
+      return "";
+    }
   },
   methods: {
     submitData(values) {
-      console.log(values);
+      this.isLoading = true;
+      this.error = "";
+      const signinDO = {
+        email: values.email,
+        password: values.password,
+        returnSecureToken: true,
+      };
+      // Web-API-Schlüssel : AIzaSyC77iv6arToXhKW-GVqtakHEwfT7W7DP8Q 
+      axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`, 
+        signinDO
+      )
+      .then((response) => {
+        console.log(response);
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        this.error = error.response.data.error.message;
+        console.log(this.error);
+        this.isLoading = false;
+      });
     },
     changeComponent(componentName) {
       this.$emit("change-component", { componentName });
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -12,6 +12,9 @@
         >
       </p>
     </div>
+    <div class="alert alert-danger col-md-8 offset-2" v-if="error">
+      {{ errorDisplayText }}
+    </div>
     <Form @submit="submitData" :validation-schema="schema" v-slot="{ errors }">
       <div class="form-row">
         <div class="form-group col-md-8 offset-2">
@@ -63,7 +66,10 @@
       <div class="form-row mt-3">
         <div class="form-group col-md-8 offset-2">
           <div class="d-grid">
-            <button class="btn bg-vue">Registrieren</button>
+            <button class="btn bg-vue">
+              <span v-if="!isLoading">Registrieren</span>
+              <span v-else class="spinner-border spinner-border-sm"></span>
+            </button>
           </div>
         </div>
       </div>
@@ -73,6 +79,8 @@
 <script>
 import { Form, Field } from "vee-validate";
 import * as yup from "yup";
+import axios from "axios";
+import { FIREBASE_API_KEY } from "../../config/firebase";
 
 export default {
   name: "Register",
@@ -106,11 +114,44 @@ export default {
     });
     return {
       schema,
+      error: "",
+      isLoading: false,
     };
+  },
+  computed: {
+    errorDisplayText() {
+      if (this.error) {
+        if (this.error.includes("EMAIL_EXISTS")) {
+          return "Die EMail Adress existiert bereits";
+        }
+        return "Unbekannte Fehler. Bitte versuchen Sie es noch einmal";
+      }
+      return "";
+    }
   },
   methods: {
     submitData(values) {
-      console.log(values);
+      this.isLoading = true;
+      this.error = "";
+      const signupDO = {
+        email: values.email,
+        password: values.password,
+        returnSecureToken: true,
+      };
+      // Web-API-Schlüssel : AIzaSyC77iv6arToXhKW-GVqtakHEwfT7W7DP8Q 
+      axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`, 
+        signupDO
+      )
+      .then((response) => {
+        console.log(response);
+        this.isLoading = false;
+        this.changeComponent('login');
+      })
+      .catch((error) => {
+        this.error = error.response.data.error.message;
+        this.isLoading = false;
+      });
     },
     changeComponent(componentName) {
       this.$emit("change-component", {componentName});
